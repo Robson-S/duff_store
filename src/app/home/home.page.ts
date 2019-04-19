007ac14687bce1d86acf01d3aaad960ee3bf6ff2
ROBSON SILVA BERNALDO <496050825@alunos.etec.itaqua>
2019-04-18 22:33:36 -0300

    Criado tela de cadastro de cervejas.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
+  cervejas;
+
+  ngOnInit(){
+ 
+  }
+
+  ionViewDidEnter(){
+    this.cervejas = [];
+
+    const chavesDisponiveis = sessionStorage.getItem('chaves');
+    
+    const chavesSeparadas = chavesDisponiveis.split(';')
+
+    for(var i = 0; i < chavesSeparadas.length; i++){
+      const cerveja = sessionStorage.getItem(chavesSeparadas[i])
+      const cervejasObj = JSON.parse(cerveja)
+      this.cervejas.push(cervejasObj)
+  }
+}
 }
